0024e08c33fefe1b23df5149c54fe7a4ec145fb3
jia <ggd599@gmail.com>
2022-08-22 20:25:17 +0900

    guest/join/은행실명인증 api 반 성공08.22_인아

--- hotel/src/main/java/kr/co/hotel/guest/GuestController.java
+++ hotel/src/main/java/kr/co/hotel/guest/GuestController.java
@@ -10,10 +10,10 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class GuestController {
@@ -207,4 +207,11 @@ public class GuestController {
 			 out.flush();
 		 		 
 	 }
+	 @PostMapping(
+			  	path = "/v2.0/inquiry/real_name", 
+			  	consumes = "application/x-www-form-urlencoded", 
+			  	produces = "application/json")
+	 public void bankCheck(){
+			  
+	 };
 }
