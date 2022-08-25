43e8f6a5b62f7c8e7905f5241bbda437b48572e5
jia <ggd599@gmail.com>
2022-08-25 14:27:44 +0900

    게스트 회원가입 계좌실명인증 일부성공08.25_인아

---
+++ hotel/src/main/java/kr/co/hotel/guest/ApiVO.java
@@ -0,0 +1,13 @@
+package kr.co.hotel.guest;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApiVO {
+	
+	private String bank_code_std;
+	private String account_num;
+	private String account_holder_info;
+}
